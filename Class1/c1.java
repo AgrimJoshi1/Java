276b338d27b4653d8e3c2b79fed6628835f138e4
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-07-01 09:22:06 +0530

    class1

---
+++ Class1/c1.java
@@ -0,0 +1,20 @@
+package Class1;
+public class c1 {
+    //void - type,
+    public static void main(String[] args) {
+        System.out.println("Agrim Joshi");
+        System.out.print("Hello \nWorld");
+
+        int n = 10;
+        if(n<=10){
+            System.out.println("n is less than 10");
+        }
+        else{
+            System.out.println("n is greater than 10");
+        }
+        }
+        
+        
+    }
+    
+
---
+++ Class1/c2.java
@@ -0,0 +1,30 @@
+package Class1;
+
+public class c2 {
+    public static void main(String[] args) {
+    // for(int i = 0; i<=5;i++){
+    //     for(int j =0; j<=5;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println("");
+    // }
+
+    // for(int i = 0; i<= 5;i++){
+    //     for(int j = 0;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println("");
+    // }
+
+    // for(int i = 5; i>=1;i--){
+    //     for(int j = 1;j<=5-i;j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1;j<=i;j++){
+    //         System.out.print("*");
+    //     }
+    //     System.out.println("");
+    // }
+}
+    
+}
