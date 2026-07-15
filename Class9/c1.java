bc14f589b0c38796a5c38ee7ac62aa7d297346d4
AgrimJoshi1 <AgrimJoshi238@gmail.com>
2026-07-15 23:20:02 +0530

    Class9

---
+++ Class9/c1.java
@@ -0,0 +1,13 @@
+package Class9;
+
+public class c1 {
+    void printMessage(String name) {
+        System.out.println("Hello" + name);
+    }
+
+    public static void main(String[] args) {
+        c1 obj = new c1();
+        obj.printMessage(" Noor" );
+    }
+}
+
